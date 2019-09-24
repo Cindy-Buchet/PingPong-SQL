5a35d738df066e655eb4b51535fc2ba0e62bf3a5
Cindy Buchet <mllcindy@hotmail.com>
2019-09-24 16:46:48 +0200

    partie connexion ok

--- login.php
+++ login.php
@@ -5,47 +5,54 @@
     <title>Login</title>
     <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
   </head>
-  <body>
+  <body>echo
   <?php
     $login = $_POST['login'];
     $pwd = $_POST['pwd'];
-    include('index.htm');
+    session_start();
 
     $bdd = new PDO('mysql:host=localhost;dbname=pingpong;charset=utf8', 'root', 'root');	
 
-    if (isset($_POST['login']) && isset($_POST['pwd'])) {
-      
-      $req = $bdd->prepare('SELECT id FROM users WHERE pseudo = :pseudo AND pwd = :pwd');
-      
-      $req->execute(array(
-        'pseudo' => $login,
-        'pwd' => $pwd));
-      $resultat = $req->fetch();
-
-      if (!$resultat) {
-        // Mauvais mdp ou pseudo
-        echo 'Mauvais identifiant ou mot de passe !';
+    // Vérifier que l'utilisateur existe
+    if (isset($_POST['connexion'])){
         
+      if (isset($_POST['login']) && isset($_POST['pwd'])) {
+        
+        $req = $bdd->prepare('SELECT id FROM users WHERE pseudo = :pseudo AND pwd = :pwd');
+        
+        $req->execute(array(
+          'pseudo' => $login,
+          'pwd' => $pwd));
+        $resultat = $req->fetch();
+
+        if (!$resultat) {
+          // Mauvais mdp ou pseudo
+          $_SESSION['resultat'] =  'Mauvais identifiant ou mot de passe !';
+          header('location:index.php');
+        }
+        
+        else {
+          // dans ce cas, tout est ok, on peut démarrer notre session
+
+          // on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
+          $_SESSION['login'] = $_POST['login'];
+          $_SESSION['pwd'] = $_POST['pwd'];
+          $_SESSION['resultat'] = 'Vous êtes connecté !';
+
+          
+          // on redirige notre visiteur vers une autre page
+          header ('location: read.php');
+        }
+
       }
-      
       else {
-    		// dans ce cas, tout est ok, on peut démarrer notre session
-
-    		session_start ();
-    		// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
-    		$_SESSION['login'] = $_POST['login'];
-    		$_SESSION['pwd'] = $_POST['pwd'];
-        echo 'Vous êtes connecté !';
-
-        
-    		// on redirige notre visiteur vers une autre page
-    		header ('location: read.php');
+        echo 'Les variables du formulaire ne sont pas déclarées.';
       }
-
-    
     }
-    else {
-    	echo 'Les variables du formulaire ne sont pas déclarées.';
+
+    if (isset($_POST['inscription'])){
+      $_SESSION['inscri'] = "Vous devez vous inscrire";
+      header ('location: index.php');
     }
     ?>
   </body>
